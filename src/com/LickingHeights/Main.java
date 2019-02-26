badf2b34b7a2ca13b2ee3b279490b019f95edf96
Unknown <lucy.kasler.2022@student.lhschools.org>
2019-02-26 13:46:41 -0500

    Declared Variables

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -1,8 +1,16 @@
 package com.LickingHeights;
 
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
+
+        Scanner keyboard;
+        int value;
+        int testing;
+        String queston;
+        
 
     }
 }
