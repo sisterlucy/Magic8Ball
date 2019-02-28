9cb7c6e3d8d15536f3619e47ae156483be5d3e23
Unknown <lucy.kasler.2022@student.lhschools.org>
2019-02-28 13:29:24 -0500

    Added if and else if statements

--- src/com/LickingHeights/Main.java
+++ src/com/LickingHeights/Main.java
@@ -12,8 +12,20 @@ public class Main {
         String queston;
         keyboard= new Scanner(System.in);
 
-        System.out.println("Ask a  yes or no question.");
-        keyboard.nextLine();
+        System.out.println("Think of a question, then pick a number between 0-100");
+        value = keyboard.nextInt();
+        if (value<=10){
+            System.out.println("Ask again");
+        }
+        else if (value>=10 &&value<20){
+            System.out.println("Definiltey");
+        }
+
+
+
+
+
+
 
     }
 }
